e6b0ebe9ce11052a7bb4e30dc5ca555a8eed3935
hankarun <hankarun@gmail.com>
2025-06-19 22:27:29 +0300

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@ int main()
     
     // Define the camera
     Camera3D camera = { 0 };
-    camera.position = Vector3{ 0.0f, 1.0f, -3.0f };  // Camera position
+    camera.position = Vector3{ -1.0f, 1.0f, -3.0f };  // Camera position
     camera.target = Vector3{ 0.0f, 0.0f, 0.0f };    // Camera looking at point
     camera.up = Vector3{ 0.0f, 1.0f, 0.0f };        // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                            // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;         // Camera projection type
     
     // Define the light position (in world space)
-    Vector3 lightPos = { 5.0f, 0.0f, 0.0f };
+    Vector3 lightPos = { 5.0f, 3.0f, 0.0f };
     
     // Create Earth celestial body
     CelestialBody earth("Earth", 1.0f, 10.0f); // Name, radius, rotation speed
