d190d0e4f31a6d85b824e06e432f8e1b4cda450b
ali turan <ali.turan@simsoft.com.tr>
2025-06-19 12:05:14 +0300

    Add mesh

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,33 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 600;
     
-    InitWindow(screenWidth, screenHeight, "Path Tracer");
+    InitWindow(screenWidth, screenHeight, "Path Tracer");    // Define the camera
+    Camera3D camera = { 0 };
+    camera.position = Vector3{ 2.0f, 0.0f, 2.0f };  // Camera position
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };    // Camera looking at point
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };        // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                            // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;         // Camera projection type
     
     SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
     
+    Vector3 spherePosition = { 0.0f, 0.0f, 0.0f }; // Position of the sphere
+    float sphereRadius = 1.0f;                    // Radius of the sphere
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         // TODO: Update your variables here
-        
-        // Draw
+          // Draw
         BeginDrawing();
         
             ClearBackground(RAYWHITE);
             
-            DrawText("Welcome to Raylib!", 190, 200, 40, MAROON);
-            DrawCircle(screenWidth/2, screenHeight/2 + 100, 50, DARKBLUE);
+            BeginMode3D(camera);
+                DrawSphere(spherePosition, sphereRadius, RED);                
+            EndMode3D();
+            
+            DrawText("Camera at (2,0,2) looking at (0,0,0)", 10, 40, 20, BLACK);
         
         EndDrawing();
     }
